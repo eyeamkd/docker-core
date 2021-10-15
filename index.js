6957715596782fe0f67a16d20955bc1ad75532bf
Kunal Dubey <kunaldubey2297@gmail.com>
2021-10-15 18:44:56 +0530

    able to make successfull connection to the database

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express  = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express() 
+const app = express();
+
+mongoose
+  .connect("mongodb://eyeamkd:edo-okati@172.29.0.2:27017/?authSource=admin")
+  .then(() => console.log("Successfully connected to the database, Yeayy!"))
+  .catch( (err)=>console.log("Error connecting to the same database ",err));
 
 const port = process.env.PORT || 3000;
 
-app.get('/',(req,res)=>{
-    res.send("<h1>Hello! Kunal Dubey Here checking production status </h1>")
-}) 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello! Kunal Dubey Here checking production status </h1>");
+});
 
-app.listen(port,()=> console.log("Server started!!"))
+app.listen(port, () => console.log("Server started!!"));
