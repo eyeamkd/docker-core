1d4ff4f802ba15039bd6b4d013557f05f93c8ebd
Kunal Dubey <kunaldubey2297@gmail.com>
2021-10-15 13:06:51 +0530

    docker volume mounts

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
-    res.send("<h1>Hello!</h1>")
+    res.send("<h1>Hello! Kunal Dubey Here </h1>")
 }) 
 
 app.listen(port,()=> console.log("Server started!!"))
