c05f150af3134dadbbfb3f64bdddbe6526d9b2da
Kunal Dubey <kunaldubey2297@gmail.com>
2021-10-15 19:18:14 +0530

    moved mongo config to env

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); 
+const {MONGO_PASSWORD,MONGO_PORT,MONGO_USER,MONGO_IP} = require("./config/config"); 
 
 const app = express();
 
 mongoose
-  .connect("mongodb://eyeamkd:edo-okati@172.29.0.2:27017/?authSource=admin")
+  .connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
   .then(() => console.log("Successfully connected to the database, Yeayy!"))
   .catch( (err)=>console.log("Error connecting to the same database ",err));
 
